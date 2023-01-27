9b1feb5f7e1612e7199c3348ea49124f3c306e43
NickoDob <kolyadob@gmail.com>
2023-01-27 16:40:46 +0500

    1

---
+++ controllers.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import pathlib
+
+from flask import request
+from dbfread import DBF
+from pymsgbox import alert
+
+
+def check_page_creator(gt):
+    keylist, checklist = [], set()
+    for column1 in gt[0]:
+        for column2 in gt[1]:
+            if column2 == column1:
+                keylist.append(column2)
+            checklist.add(column2)
+    keys = keylist
+    checks = [elem for elem in checklist if elem not in keylist]
+    return [gt, keys, checks]
+
+
+def validator(gt):
+    if isinstance(gt, str):
+        return alert("Выбран файл с неподходящим расширением", "Ошибка", button='OK')
+    else:
+        return check_page_creator(gt)
+
+
+def checkboxes(taglist):
+    checklist = []
+    for checkbox in taglist:
+        value = request.form.get(checkbox)
+        if value:
+            checklist.append(checkbox)
+    return checklist
+
+
+def keyboxes(taglist):
+    keylist = []
+    for keybox in taglist:
+        value = request.form.get(keybox)
+        if value:
+            keylist.append(keybox)
+    return keylist
+
+
+def check_replacer(df):
+    value = request.form.get('Замена')
+    if value:
+        for column in df:
+            df[column] = df[column].astype(str).str.replace('ё', 'е')
+    return df
+
+
+def global_table():
+    data = []
+    files = [request.files['file1'], request.files['file2']]
+    for file in files:
+        if pathlib.Path(file.filename).suffix == '.xlsx':
+            file.save('./tables/' + file.filename)
+            df = pd.read_excel('./tables/' + file.filename, parse_dates=False, index_col=None)
+            df.columns = [column.strip() for column in df]
+            data.append(df)
+        elif pathlib.Path(file.filename).suffix == '.csv':
+            file.save('./tables/' + file.filename)
+            df = pd.read_csv('./tables/' + file.filename, sep=";", encoding="cp1251", index_col=None)
+            df.columns = [column.strip() for column in df]
+            data.append(df)
+        elif pathlib.Path(file.filename).suffix == '.dbf':
+            file.save('./tables/' + file.filename)
+            df = pd.DataFrame(iter(DBF('./tables/' + file.filename)))
+            df.columns = [column.strip() for column in df]
+            data.append(df)
+        else:
+            return 'Ошибка'
+    return data
---
+++ main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+from flask import Flask, render_template, redirect, url_for, request
+
+from controllers import global_table, checkboxes, keyboxes, check_replacer, validator
+
+
+app = Flask(__name__)
+
+gt, keys, checks = [], [], []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    global gt, keys, checks
+    if request.method == 'POST':
+        gt = global_table()
+        val = validator(gt)
+        if isinstance(val, list):
+            keys, checks = val[1], val[2]
+            return render_template('checkpage.html', keylist=keys, checklist=checks)
+        else:
+            return redirect(url_for('.upload'))
+    return render_template('index.html')
+
+
+@app.post('/coincidences')
+def coincidences():
+    global gt, keys
+    df = pd.concat(gt)
+    check_replacer(df)
+    keyslist = keyboxes(keys)
+    coinc = df[df.duplicated(keyslist, keep=False)].reset_index(drop=True)
+    coinc.to_excel('./results/coincidences.xlsx')
+    return render_template('result.html', tables=[coinc.to_html(classes='mystyle')])
+
+
+@app.post('/differences')
+def differences():
+    global gt, keys
+    df = pd.concat(gt)
+    check_replacer(df)
+    keyslist = keyboxes(keys)
+    diff = df.drop_duplicates(subset=keyslist, keep=False).reset_index(drop=True)
+    diff.to_excel('./results/differences.xlsx')
+    return render_template('result.html', tables=[diff.to_html(classes='mystyle')])
+
+
+@app.post('/joined')
+def joined():
+    global gt, keys, checks
+    keyslist = keyboxes(keys)
+    checklist = checkboxes(checks)
+    df = pd.merge(gt[0], gt[1][keyslist + checklist], how='left', left_on=keyslist, right_on=keyslist)
+    check_replacer(df)
+    df.to_excel('./results/joined.xlsx')
+    return render_template('result.html', tables=[df.to_html(classes='mystyle')])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
