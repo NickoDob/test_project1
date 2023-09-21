f37610a17e1fa4826371606a0ee8494b49158dbc
NickoDob <54015658+NickoDob@users.noreply.github.com>
2023-09-21 11:48:04 +0500

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -18,3 +18,10 @@ function deRequire(elClass) {
     }
   }
 }
+
+function toggle(source, elClass) {
+  checkboxes = document.getElementsByClassName(elClass);
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
